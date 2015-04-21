bf59e78e8e6d3bdfc38a0f95c210c4798cb59a3b
Zak Koltun <zakkoltun@gmail.com>
2015-04-21 09:01:32 -0700

    Add arel gem to Gemfile, fix migration error

---
+++ db/schema.rb
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150421110806) do
+
+  create_table "slugs", force: true do |t|
+    t.integer  "sluggable_id",                              null: false
+    t.string   "sluggable_type",                            null: false
+    t.boolean  "active",                     default: true, null: false
+    t.string   "slug",           limit: 126,                null: false
+    t.string   "scope",          limit: 126
+    t.datetime "created_at"
+  end
+
+  add_index "slugs", ["sluggable_type", "scope", "slug"], name: "slugs_unique", unique: true
+  add_index "slugs", ["sluggable_type", "sluggable_id", "active"], name: "slugs_for_record"
+
+end
